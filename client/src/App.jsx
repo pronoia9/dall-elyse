ff33a28f0cbc2c900b978833b845d72c96b6629a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-17 20:49:00 +0300

    add logo link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { Home, CreatePost } from './pages';
+import { logo } from './assets';
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-28 object-contain' />
+        </Link>
+      </header>
+    </BrowserRouter>
+  );
 }
 
 export default App;
