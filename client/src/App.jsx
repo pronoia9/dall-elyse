2557a78bd2abd792c46d3b0e01f69cf8b3d64cd8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-17 22:01:20 +0300

    change color scheme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,16 @@ import { logo } from './assets';
 function App() {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-[#2c2c32] sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between items-center bg-[#001e38] sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
         <Link to='/'>
           <img src={logo} alt='logo' className='w-28 object-contain' />
         </Link>
-        <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+        <Link to='/create-post' className='font-inter font-medium bg-[#4a6d88] text-[#d5d9e0] px-4 py-2 rounded-md'>
           Create
         </Link>
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#0b0b14] text-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#0b0b14] text-[#d5d9e0] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create-post' element={<CreatePost />} />
