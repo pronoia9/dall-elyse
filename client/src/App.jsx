4bad94050ec640e2f30f15c1fcce43bf43525b89
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-17 21:36:52 +0300

    change bg/text colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { logo } from './assets';
 function App() {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between items-center bg-[#2c2c32] sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
         <Link to='/'>
           <img src={logo} alt='logo' className='w-28 object-contain' />
         </Link>
@@ -16,7 +16,7 @@ function App() {
         </Link>
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#0b0b14] text-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create-post' element={<CreatePost />} />
