17ff2817d2dd3061f921147aae3988b4f2347c8d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-18 00:46:50 +0300

    change styles

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,5 +1,30 @@
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
+  return (
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label htmlFor={name} className='block text-sm font-medium text-[#d5d9e0]'>
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#4a6d88] py-1 px-2 rounded-[5px] text-[#d5d9e0]'>
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        className='bg-[#001e3825] border border-[#001e38] text-[#d5d9e0] text-sm rounded-lg focus:ring-[#4a6d88] focus:border-[#4a6d88] outline-none block w-full p-3'
+      />
+    </div>
+  );
 };
 
 export default FormField;
