bfe3dc0eb4fa47a75602605f7ecbe115e5acef5f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-18 04:32:43 +0300

    fix typo, change messages

--- server/index.js
+++ server/index.js
@@ -12,17 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-  res.send('Hello from DALL-E.');
+ res.status(200).json({ message: 'Hello from DALL.E!', });
 });
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server is running on http://localhost:8080...'));
+    app.listen(8080, () => console.log('DALL-Elyse is listening...'));
   } catch (error) {
     console.log('Error starting server.', error);
   }
