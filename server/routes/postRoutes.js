234ea0f18e0ba5748f66c9cf23d5fc229fd48d80
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-18 04:30:04 +0300

    change error messages

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -20,7 +20,7 @@ router.route('/').get(async (req, res) => {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
   } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
   }
 });
 
@@ -30,9 +30,9 @@ router.route('/').post(async (req, res) => {
     const { name, prompt, photo } = req.body;
     const photoUrl = await cloudinary.uploader.upload(photo);
     const newPost = await Post.create({ name, prompt, photo: photoUrl.url });
-    res.status(201).json({ success: true, data: newPost });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error });
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
   }
 });
 
