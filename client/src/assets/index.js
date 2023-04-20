db7948a9c6f049adbaa541063892e880723a9903
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-21 02:28:16 +0300

    add new images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,8 +1,11 @@
+import artist from './artist.png';
 import close from './close.svg';
 import closeFilled from './close-filled.svg';
+import creative from './creative.png';
 import download from './download.png';
+import ideate from './ideate.png';
 import logo from './logo.png';
 import menu from './menu.svg';
 import preview from './preview.png';
 
-export { close, closeFilled, download, logo, menu, preview };
+export { artist, close, closeFilled, creative, download, ideate, logo, menu, preview };
