d977e851b8ce18304f50b6ace14b79478564b90c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-21 00:25:49 +0300

    update index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,13 @@
-import Card from './Card';
+import About from './About';
+import Contact from './Contact';
+import Create from './Create';
 import Footer from './Footer';
 import FormField from './FormField';
+import Gallery from './Gallery';
+import GalleryCard from './GalleryCard';
+import Hero from './Hero';
 import Loader from './Loader';
 import Navbar from './Navbar';
+import NavbarItem from './NavbarItem';
 
-export { Card, Footer, FormField, Loader, Navbar };
+export { About, Contact, Create, Footer, FormField, Gallery, GalleryCard, Hero, Loader, Navbar, NavbarItem };
