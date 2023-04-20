edb0d22038518f5cb81217d2f99c90ff781b4e86
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 17:04:38 +0300

    add initial navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import icon from '../assets/icon.png';
+import { navLinks } from '../utils';
+
+const NavbarLink = ({ index, link, title }) => (
+  <li id='menu-item-19' className='menu-item menu-item-type-custom menu-item-object-custom bricks-menu-item'>
+    <a href={link} aria-current='page'>
+      {title}
+    </a>
+  </li>
+);
 
 const Navbar = () => {
   return (
@@ -18,41 +27,20 @@ const Navbar = () => {
               </div>
             </div>
           </Link>
+
           <div id='brxe-ufdlom' className='brxe-div'>
             <div id='brxe-flwdpe' className='brxe-nav-menu'>
               <nav className='bricks-nav-menu-wrapper mobile_landscape'>
-                <ul id='menu-main-menu' className='bricks-nav-menu' role='menubar'>
-                  <li
-                    id='menu-item-19'
-                    className='menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-19 bricks-menu-item'
-                    role='menuitem'
-                    data-top-index='0'
-                  >
-                    <a href='/#learn-more' aria-current='page'>
-                      Learn more
-                    </a>
-                  </li>
-                  <li
-                    id='menu-item-20'
-                    className='menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-20 bricks-menu-item'
-                    role='menuitem'
-                    data-top-index='1'
-                  >
-                    <a href='/#examples' aria-current='page'>
-                      Examples
-                    </a>
-                  </li>
-                  <li
-                    id='menu-item-21'
-                    className='menu-item menu-item-type-custom menu-item-object-custom menu-item-21 bricks-menu-item'
-                    role='menuitem'
-                    data-top-index='2'
-                  >
-                    <a href='https://leonardo.ai/contact-us/'>Contact</a>
-                  </li>
+                <ul id='menu-main-menu' className='bricks-nav-menu'>
+                  {/*  */}
+                  {navLinks.map((link, index) => (
+                    <NavbarLink key={link.title} index={index} {...link} />
+                  ))}
                 </ul>
               </nav>
-              <button
+
+              {/* Mobile Menu */}
+              {/* <button
                 className='bricks-mobile-menu-toggle'
                 aria-haspopup='true'
                 aria-label='Mobile menu'
@@ -91,34 +79,25 @@ const Navbar = () => {
                   </li>
                 </ul>
               </nav>
-              <div className='bricks-mobile-menu-overlay'></div>
+              <div className='bricks-mobile-menu-overlay'></div> */}
             </div>
+
+            {/* Create Button */}
             <div
-              id='brxe-zwuywn'
-              data-script-id='zwuywn'
-              className='brxe-code'
-              data-interactions='[{"id":"hahteq","trigger":"click","action":"startAnimation","target":"popup","templateId":"2206","animationType":"slideInRight","animationDuration":".1s"}]'
-              data-interaction-id='dc3bec'
-            >
-              <svg viewBox='0 0 10 8' width='22'>
-                <path d='M1 1h8M1 4h 8M1 7h8' stroke='#fff' stroke-width='1' stroke-linecap='round'></path>
-              </svg>
-            </div>
-            <span
               id='brxe-nzeybc'
               className='brxe-button menu-request-btn bricks-button md bricks-background-primary'
-              data-interactions='[{"id":"vuefkg","trigger":"click","action":"show","target":"popup","templateId":"2159"}]'
-              data-interaction-id='ad9631'
             >
-              Create
-            </span>
+              Create 🪄
+            </div>
+
+            {/* DALLE Link */}
             <a
               id='brxe-aohtdi'
               className='brxe-button menu-launch-btn bricks-button'
-              data-interactions='[{"id":"qzvajw","trigger":"click","action":"show","target":"popup","templateId":"2441"}]'
-              data-interaction-id='7b5c63'
+              href='https://openai.com/product/dall-e-2'
+              target='_blank'
             >
-              Launch App
+              DALLE-E
             </a>
           </div>
         </div>
