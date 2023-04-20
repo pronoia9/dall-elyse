b04c87a0ec9266e23e57efb0993c2d1deb0674d6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 18:18:37 +0300

    add footer links

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -9,10 +9,28 @@ export function getRandomPrompt(prompt) {
   return randomPrompt;
 }
 
-export async function downloadImage(_id, photo) { FileSaver.saveAs(photo, `download-${_id}.jpg`); }
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
 
 export const navLinks = [
   { title: 'Vite', url: 'https://vitejs.dev' },
   { title: 'Vercel', url: 'https://vercel.com' },
   { title: 'Render', url: 'https://render.com' },
 ];
+
+export const footerLinks = {
+  about: [
+    { title: 'Learn More', url: 'https://openai.com/about' },
+    { title: 'Terms & Policies', url: 'https://openai.com/policies' },
+    { title: 'Privacy Policy', url: 'https://openai.com/policies/privacy-policy' },
+  ],
+  tuned: [
+    { title: 'Twitter', url: 'https://twitter.com/OpenAI' },
+    { title: 'YouTube', url: 'https://youtube.com/OpenAI' },
+    { title: 'GitHub', url: 'https://github.com/openai' },
+    { title: 'SoundCloud', url: 'https://soundcloud.com/openai_audio' },
+    { title: 'LinkedIn', url: 'https://www.linkedin.com/company/openai' },
+    { title: 'Discord', url: 'https://discord.com/invite/openai' },
+  ],
+};
