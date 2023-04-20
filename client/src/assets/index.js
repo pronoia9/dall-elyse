05f99f9067285b13d7fac4930d6e339bbb0c5411
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-21 02:50:15 +0300

    add new images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,8 +4,11 @@ import closeFilled from './close-filled.svg';
 import creative from './creative.png';
 import download from './download.png';
 import ideate from './ideate.png';
+import learn1 from './learn1.webp';
+import learn2 from './learn2.png';
+import learn3 from './learn3.webp';
 import logo from './logo.png';
 import menu from './menu.svg';
 import preview from './preview.png';
 
-export { artist, close, closeFilled, creative, download, ideate, logo, menu, preview };
+export { artist, close, closeFilled, creative, download, ideate, learn1, learn2, learn3, logo, menu, preview };
