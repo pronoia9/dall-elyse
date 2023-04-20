9364a51db59da08e59159e0c265eda7f19c93f2b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 23:46:28 +0300

    add temp styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './style.css';
-import './frontend.css';
+import './styles/style.css';
+import './styles/frontend.css';
+import './styles/frontend2.css';
+import './styles/vp-main.css';
+import './styles/vp-style.css';
+import './styles/vp-lazyload.css';
+import './styles/vp-pagination.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
