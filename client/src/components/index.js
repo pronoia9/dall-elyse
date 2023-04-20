4f908954bfa9f1fe530089f03c7d3b0f04d7a681
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 18:00:22 +0300

    add init footer

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 import Card from './Card';
+import Footer from './Footer';
 import FormField from './FormField';
 import Loader from './Loader';
 import Navbar from './Navbar';
 
-export { Card, FormField, Loader, Navbar };
+export { Card, Footer, FormField, Loader, Navbar };
