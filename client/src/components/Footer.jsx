c165768c45b754d1baee951cad01db47c2428fae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-20 20:17:05 +0300

    add footer component + separate navbars

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,144 +1,47 @@
+import MenuItem from './MenuItem';
+import { footerLinks } from '../utils/index';
+import logo from '../assets/icon.png';
+
+const FooterNav = ({ id, menu, title, data }) => (
+  <div id={`brxe-${id[0]}`} className='brxe-block'>
+    <div id={id[1]} className='brxe-text-basic'>
+      {title}
+    </div>
+    <div id={`brxe-${id[2]}`} className='brxe-nav-menu'>
+      <nav className='bricks-nav-menu-wrapper never'>
+        <ul id={`menu-menu-footer-brxe-${menu}`} className='bricks-nav-menu'>
+          {data.map((link, index) => (
+            <MenuItem key={link.title} index={index} {...link} />
+          ))}
+        </ul>
+      </nav>
+    </div>
+  </div>
+);
+
 const Footer = () => {
+  const { company, products, tuned } = footerLinks;
   return (
     <footer id='brx-footer' style={{ marginTop: 400 }}>
       <section id='brxe-fczqkj' className='brxe-section'>
         <div id='brxe-uvadjh' className='brxe-container'>
+          {/* Logo */}
           <div id='brxe-fdwplh' className='brxe-block'>
-            <a id='brxe-qkumnp' href='https://leonardo.ai/' className='brxe-div'>
-              <img
-                className='brxe-image css-filter size-full'
-                id='brxe-nkhgib'
-                src='https://leonardo-cdn.b-cdn.net/wp-content/uploads/2022/11/leaonardo-logo.svg'
-                alt=''
-              />
+            <a id='brxe-qkumnp' className='brxe-div' href='http://openai.com' target='_blank'>
+              <img id='brxe-nkhgib' className='brxe-image css-filter size-full' src={logo} alt='' />
               <div id='brxe-gdidui' className='brxe-div'>
                 <div id='brxe-gcvdwc' className='brxe-text-basic'>
-                  Leonardo
-                </div>
-                <div id='brxe-cbrhzw' className='brxe-text-basic rad-color'>
-                  .Ai
+                  Open{' '}
+                  <span id='brxe-cbrhzw' className='brxe-text-basic rad-color'>
+                    AI
+                  </span>
                 </div>
               </div>
             </a>
           </div>
-
-          <div id='brxe-hnfnrh' className='brxe-block'>
-            <div id='brxe-djmjoo' className='brxe-text-basic'>
-              About
-            </div>
-            <div id='brxe-gzrroy' className='brxe-nav-menu'>
-              <nav className='bricks-nav-menu-wrapper never'>
-                <ul id='menu-menu-footer-about' className='bricks-nav-menu'>
-                  <li
-                    id='menu-item-23'
-                    className='menu-item menu-item-type-custom menu-item-object-custom menu-item-23 bricks-menu-item'
-                  >
-                    <a href='#learn-more'>Learn More</a>
-                  </li>
-                  <li
-                    id='menu-item-24'
-                    className='menu-item menu-item-type-custom menu-item-object-custom menu-item-24 bricks-menu-item'
-                  >
-                    <a href='https://leonardo.ai/contact-us/'>Contact</a>
-                  </li>
-                  <li
-                    id='menu-item-822'
-                    className='menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-822 bricks-menu-item'
-                  >
-                    <a href='https://leonardo.ai/legal-notice/'>Privacy Policy</a>
-                  </li>
-                  <li
-                    id='menu-item-1305'
-                    className='menu-item menu-item-type-post_type menu-item-object-page menu-item-1305 bricks-menu-item'
-                  >
-                    <a href='https://leonardo.ai/cookie-policy-au/'>Cookie Policy (AU)</a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-
-          <div id='brxe-doknxr' className='brxe-block'>
-            <div id='brxe-nucyxi' className='brxe-text-basic'>
-              Stay tuned!
-            </div>
-            <div id='brxe-dylwoj' className='brxe-nav-menu'>
-              <nav className='bricks-nav-menu-wrapper never'>
-                <ul id='menu-menu-footer-socials' className='bricks-nav-menu'>
-                  <li
-                    id='menu-item-2328'
-                    className='menu-item menu-item-type-custom menu-item-object-custom menu-item-2328 bricks-menu-item'
-                  >
-                    <a href='https://discord.gg/leonardo-ai'>Discord</a>
-                  </li>
-                  <li
-                    id='menu-item-25'
-                    className='menu-item menu-item-type-custom menu-item-object-custom menu-item-25 bricks-menu-item'
-                    data-top-index='1'
-                  >
-                    <a target='_blank' rel='noopener' href='https://twitter.com/LeonardoAi_'>
-                      Twitter
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-
-          <div id='brxe-iyxmln' className='brxe-block'>
-            <div id='brxe-vwesws' className='brxe-text-basic'>
-              Request access 🧑‍🎨
-            </div>
-            <div id='brxe-ecgvir' className='brxe-block grid'>
-              <form id='brxe-jzefpi' className='brxe-form news-field-footer news-field' method='post'>
-                <div className='form-group'>
-                  <input
-                    id='form-field-e3b297'
-                    name='form-field-kmhhru'
-                    type='email'
-                    value=''
-                    placeholder='Email'
-                    required=''
-                  />
-                </div>
-                <div className='form-group submit-button-wrapper'>
-                  <button type='submit' className='bricks-button bricks-background-primary icon-right'>
-                    <span className='loading'>
-                      <svg
-                        version='1.1'
-                        viewBox='0 0 24 24'
-                        xmlns='http://www.w3.org/2000/svg'
-                        xmlns:xlink='http://www.w3.org/1999/xlink'
-                      >
-                        <g
-                          stroke-linecap='round'
-                          stroke-width='1'
-                          stroke='#000'
-                          fill='none'
-                          stroke-linejoin='round'
-                        >
-                          <path d='M0.927,10.199l2.787,4.151l3.205,-3.838'></path>
-                          <path d='M23.5,14.5l-2.786,-4.15l-3.206,3.838'></path>
-                          <path d='M20.677,10.387c0.834,4.408 -2.273,8.729 -6.509,9.729c-2.954,0.699 -5.916,-0.238 -7.931,-2.224'></path>
-                          <path d='M3.719,14.325c-1.314,-4.883 1.969,-9.675 6.538,-10.753c3.156,-0.747 6.316,0.372 8.324,2.641'></path>
-                        </g>
-                        <path fill='none' d='M0,0h24v24h-24Z'></path>
-                      </svg>{' '}
-                    </span>
-                    <svg className='' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 23 20'>
-                      <path
-                        fill='#fff'
-                        d='M22.3 9.608a3.182 3.182 0 0 1-.927 2.218L14.63 18.64a1.564 1.564 0 0 1-2.56-.515 1.596 1.596 0 0 1 .345-1.735l5.17-5.197H1.87c-.416 0-.816-.167-1.11-.464a1.591 1.591 0 0 1 0-2.24 1.565 1.565 0 0 1 1.11-.465h15.715l-5.17-5.213a1.592 1.592 0 0 1-.008-2.242A1.566 1.566 0 0 1 14.632.56l6.74 6.814c.591.591.925 1.395.928 2.234Z'
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </form>
-            </div>
-            <div id='brxe-nnlado' className='brxe-text-basic'>
-              By entering your e-mail address, you are agreeing to subscribe to our newsletter.
-            </div>
-          </div>
+          <FooterNav id={['hnfnrh', 'djmjoo', 'gzrroy']} menu='company' title='Company' data={company} />
+          <FooterNav id={['doknxr', 'nucyxi', 'dylwoj']} menu='products' title='Products' data={products} />
+          <FooterNav id={['iyxmln', 'nucyxi', 'dylwoj']} menu='tuned' title='Stay Tuned!' data={tuned} />
         </div>
       </section>
     </footer>
