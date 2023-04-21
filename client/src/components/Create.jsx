49ae697402947211645c7a02c316fa4fc856b5c6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-21 03:40:21 +0300

    finish new layout

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,87 +1,74 @@
-import { close } from "../assets";
+import { close } from '../assets';
 
 const Create = () => {
   return (
-    <div
-      data-popup-id='2159'
-      class='brx-popup brxe-popup-2159 brx-animated listening'
-      data-interactions='[{"id":"trbkdz","trigger":"showPopup","action":"startAnimation","animationType":"fadeIn","animationDuration":".2s"},{"id":"yvmhub","trigger":"hidePopup","action":"startAnimation","animationType":"fadeOut","animationDuration":".2s"}]'
-      data-interaction-id='f678c1'
-      // style='animation-duration: 0.2s'
-    >
-      <div class='brx-popup-content brxe-container'>
-        <section id='brxe-inqzid' class='brxe-section'>
-          <i
-            id='brxe-tzkqhm'
-            class='brxe-icon ti-close'
-            data-interactions='[{"id":"zhvwtj","trigger":"click","action":"hide","target":"popup","targetSelector":".btx-popup","templateId":"2159"},{"id":"bydfpo","trigger":"mouseover","action":"startAnimation","animationType":"bounce","animationDuration":"Fast"}]'
-            data-interaction-id='6e0f9f'
-          >
+    <div className='brx-popup brxe-popup-2159 brx-animated listening'>
+      <div className='brx-popup-content brxe-container'>
+        <section id='brxe-inqzid' className='brxe-section'>
+          <p id='brxe-tzkqhm' className='brxe-icon ti-close'>
             x
-          </i>
-          <div id='brxe-dekhcm' data-script-id='dekhcm' class='brxe-code'>
+          </p>
+          <div id='brxe-dekhcm' className='brxe-code'>
             <h1 align='center'>
-              Use <span class='rad-color'>DALL-Elyse</span>
+              Use <span className='rad-color'>DALL-Elyse</span>
             </h1>
             <br />
-            <h2 class='txt-center'>
-              Create a stunning and imaginative image with the help of AI and share it with the community.
-            </h2>
+            <h3 className='txt-center'>
+              Create a stunning images with the help of AI and share it with the community.
+            </h3>
+            <br />
+            <br />
           </div>
-          <div id='brxe-wujkiw' class='brxe-text-basic'>
-            Once you have created the image you want, you can share it with others in the community.
-          </div>
-          <div id='brxe-liisel' class='brxe-block grid'>
-            <form
-              id='brxe-nwxeps'
-              data-script-id='nwxeps'
-              class='brxe-form news-field news-field-main'
-              method='post'
-              data-element-id='nwxeps'
-            >
-              <div class='form-group'>
-                <input
-                  id='form-field-08e6c6'
-                  name='form-field-kmhhru'
-                  type='email'
-                  value=''
-                  placeholder='Email'
-                  required=''
-                />
+          <div id='brxe-liisel' className='brxe-block grid'>
+            <form>
+              <div id='brxe-nwxeps' className='brxe-form news-field news-field-main'>
+                <div className='form-group'>
+                  <input
+                    id='form-field-08e6c6'
+                    name='form-field-kmhhru'
+                    type='email'
+                    value=''
+                    placeholder='Email'
+                    required=''
+                  />
+                </div>
               </div>
-
-              <div class='form-group submit-button-wrapper'>
-                <button type='submit' class='bricks-button bricks-background-primary'>
-                  <span class='text'>Count me in 🤝</span>
-                  <span class='loading'>
-                    <svg
-                      version='1.1'
-                      viewBox='0 0 24 24'
-                      xmlns='http://www.w3.org/2000/svg'
-                      xmlns:xlink='http://www.w3.org/1999/xlink'
-                    >
-                      <g
-                        stroke-linecap='round'
-                        stroke-width='1'
-                        stroke='#000'
-                        fill='none'
-                        stroke-linejoin='round'
-                      >
-                        <path d='M0.927,10.199l2.787,4.151l3.205,-3.838'></path>
-                        <path d='M23.5,14.5l-2.786,-4.15l-3.206,3.838'></path>
-                        <path d='M20.677,10.387c0.834,4.408 -2.273,8.729 -6.509,9.729c-2.954,0.699 -5.916,-0.238 -7.931,-2.224'></path>
-                        <path d='M3.719,14.325c-1.314,-4.883 1.969,-9.675 6.538,-10.753c3.156,-0.747 6.316,0.372 8.324,2.641'></path>
-                      </g>
-                      <path fill='none' d='M0,0h24v24h-24Z'></path>
-                    </svg>
-                  </span>
-                </button>
+              <br />
+              <div id='brxe-nwxeps' className='brxe-form news-field news-field-main'>
+                <div className='form-group'>
+                  <input
+                    id='form-field-08e6c6'
+                    name='form-field-kmhhru'
+                    type='email'
+                    value=''
+                    placeholder='Email'
+                    required=''
+                  />
+                </div>
+                <div className='form-group submit-button-wrapper'>
+                  <button className='bricks-button bricks-background-primary'>
+                    <span className='text'>Surprise Me 🎁</span>
+                  </button>
+                </div>
               </div>
             </form>
+          </div>
+          <div id='brxe-wujkiw' className='brxe-text-basic'>
+            Once you have created the image you want, you can share it with others in the community.
+          </div>
+          <br />
+          <div id='brxe-liisel' className='brxe-block grid'>
+            <div id='brxe-nwxeps' className='brxe-form news-field news-field-main' style={{ padding: 0 }}>
+              <div className='form-group submit-button-wrapper'>
+                <button className='bricks-button bricks-background-primary'>
+                  <span className='text'>Share with the community</span>
+                </button>
+              </div>
+            </div>
           </div>
         </section>
       </div>
     </div>
   );
-}
-export default Create
+};
+export default Create;
