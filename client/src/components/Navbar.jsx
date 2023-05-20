381ba9fb22f310187db5e615b9efbe514a73ece7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 21:50:29 +0300

    add hamburger menu + styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
-import { navlinks } from '../utils/data';
+import { navbarData } from '../utils/data';
 
 const NavLink = ({ title, url }) => (
   <NavListItem>
@@ -12,7 +12,7 @@ const NavLink = ({ title, url }) => (
 );
 
 export default function Navbar() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <Container>
@@ -20,22 +20,36 @@ export default function Navbar() {
         {/* Logo */}
         <Logo>
           <a>
-            <img src='/src/assets/logo.png' alt='logo' />
+            <img src={navbarData.logo} alt='logo' />
           </a>
         </Logo>
 
         {/* Navigation Links */}
+        {/* Desktop Menu */}
         <NavWrapper>
           <Nav>
             <NavList>
-              {navlinks.map((link, index) => (
-                <NavLink key={link.title} {...link} />
+              {navbarData.navlinks.map((link) => (
+                <NavLink key={`navbar-${link.title}`} {...link} />
               ))}
             </NavList>
           </Nav>
+          {/* Mobile Menu Hamburger Icon */}
+          {!mobileMenuOpen && (
+            <MobileMenuHamburger onClick={() => setMobileMenuOpen((prev) => !prev)}>
+              <i className='fa-solid fa-bars' />
+            </MobileMenuHamburger>
+          )}
         </NavWrapper>
 
-        {isMobile && <MobileMenu></MobileMenu>}
+        {/* Mobile Menu */}
+        {mobileMenuOpen && (
+          <MobileMenu>
+            {navbarData.navlinks.map((link) => (
+              <NavLink key={`navbar-${link.title}`} {...link} />
+            ))}
+          </MobileMenu>
+        )}
       </Wrapper>
     </Container>
   );
@@ -60,9 +74,17 @@ const Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 50px;
+
+  @media only screen and (max-width: 960px) {
+    padding: 30px 40px;
+  }
+
+  @media only screen and (max-width: 760px) {
+    padding: 20px 20px;
+  }
 `;
 
-/* --- Logo --- */
+/* --- LOGO --- */
 const Logo = styled.div`
   a {
     display: block;
@@ -79,13 +101,17 @@ const Logo = styled.div`
   }
 `;
 
-/* --- Navigation --- */
+/* --- DESKTOP NAVIGATION --- */
 const NavWrapper = styled.div`
   display: flex;
   flex-direction: row;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  @media only screen and (max-width: 960px) {
+    display: none;
+  }
+`;
 
 const NavList = styled.ul`
   padding: 0;
@@ -106,7 +132,6 @@ const NavListItem = styled.li`
     color: rgba(255, 255, 255, 0.5);
     font-size: 12px;
     line-height: 29px;
-    font-family: 'Montserrat', sans-serif;
     font-weight: 500;
     text-transform: uppercase;
     text-decoration: none;
@@ -119,6 +144,37 @@ const NavListItem = styled.li`
       color: rgba(255, 255, 255, 1);
     }
   }
+
+  @media only screen and (max-width: 960px) {
+    margin: 0 0 0 30px;
+  }
 `;
 
-const MobileMenu = styled.div``;
+/* --- MOBILE MENU --- */
+const MobileMenuHamburger = styled.div`
+  @media only screen and (max-width: 960px) {
+    display: block;
+    width: 30px;
+    height: 30px;
+    font-size: 32px;
+    line-height: 30px;
+  }
+`;
+
+const MobileMenu = styled.div`
+  display: none;
+
+  a {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    color: #ffffff;
+    text-transform: uppercase;
+    font-size: 24px;
+    line-height: 29px;
+    margin: 0 0 18px 0;
+  }
+
+  @media only screen and (max-width: 960px) {
+    display: block;
+  }
+`;
