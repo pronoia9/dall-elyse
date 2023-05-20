efa0c061f2cd5ea8cd899585ef9c88e5665c6f6a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 19:09:20 +0300

    add initial layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,43 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <Container>
+      <Wrapper>
+        {/* Logo */}
+        <Logo>
+          <a>
+            <img />
+          </a>
+        </Logo>
+
+        {/* Navigation Links */}
+        {isMobile ? (
+          <>
+            {/* Desktop */}
+            <Nav></Nav>
+          </>
+        ) : (
+          <>
+            {/* Mobile Menu */}
+            <MobileMenu></MobileMenu>
+          </>
+        )}
+      </Wrapper>
+    </Container>
+  );
+};
+export default Navbar;
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const Logo = styled.div``;
+
+const Nav = styled.nav``;
+
+const MobileMenu = styled.div``;
