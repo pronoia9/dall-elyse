be579ee8af3a37a790504d854f294cee2b516a4d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 00:07:33 +0300

    add global styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Navbar } from './components';
 import { HomePage } from './pages';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
   return (
     <>
+      <GlobalStyles />
       <Navbar />
       <HomePage />
     </>
