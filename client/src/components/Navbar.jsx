3e39e6e909fe4c01899c2f0d8e6b32aec6151d77
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 19:27:42 +0300

    restructure + map navlinks

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Navbar = () => {
+import { navlinks } from '../utils/data';
+
+const NavLink = ({ title, url }) => (
+  <NavListItem>
+    <a href={url} target='_blank'>
+      {title}
+    </a>
+  </NavListItem>
+);
+
+export default function Navbar() {
   const [isMobile, setIsMobile] = useState(false);
 
   return (
@@ -15,29 +25,51 @@ const Navbar = () => {
         </Logo>
 
         {/* Navigation Links */}
-        {isMobile ? (
-          <>
-            {/* Desktop */}
-            <Nav></Nav>
-          </>
-        ) : (
-          <>
-            {/* Mobile Menu */}
-            <MobileMenu></MobileMenu>
-          </>
-        )}
+        <NavWrapper>
+          <Nav>
+            <NavList>
+              {navlinks.map((link, index) => (
+                <NavLink key={link.title} {...link} />
+              ))}
+            </NavList>
+          </Nav>
+        </NavWrapper>
+
+        {isMobile && <MobileMenu></MobileMenu>}
       </Wrapper>
     </Container>
   );
-};
-export default Navbar;
+}
 
-const Container = styled.div``;
+// STYLES
+// Animation
 
-const Wrapper = styled.div``;
+// Styles
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  transition: z-index 0.5s, transform 0.5s;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px;
+`;
 
 const Logo = styled.div``;
 
+const NavWrapper = styled.div``;
+
 const Nav = styled.nav``;
+
+const NavList = styled.ul``;
+
+const NavListItem = styled.li``;
 
 const MobileMenu = styled.div``;
