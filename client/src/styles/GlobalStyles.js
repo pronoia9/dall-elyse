c84ae38009bc352e06fec8c4ff5c36f374fc5d64
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 00:07:27 +0300

    move styles to global styles

---
+++ client/src/styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  body,
+  html {
+    padding: 0;
+    margin: 0;
+    min-height: 100%;
+  }
+
+  body {
+    overflow-x: hidden;
+    background: #000000;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 16px;
+    line-height: 28px;
+    font-weight: 500;
+    color: #808080;
+  }
+`;
