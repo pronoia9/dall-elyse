c5a9eb2f87eb8f0b01a3d031e2a324828433a5a0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 18:12:53 +0300

    add init loader

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,9 @@
 import Canvas from './Canvas';
 import Gallery from './Gallery';
 import GalleryCard from './GalleryCard';
+import Loader from './Loader';
 import Navbar from './Navbar';
 import NavbarItem from './NavbarItem';
 import Navigation from './Navigation';
 
-export { Canvas, Gallery, GalleryCard, Navbar, NavbarItem, Navigation };
+export { Canvas, Gallery, GalleryCard, Loader, Navbar, NavbarItem, Navigation };
