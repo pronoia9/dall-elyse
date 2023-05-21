ef590675a658c62391e09c8776a0b49830b46577
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 18:42:57 +0300

    move text styles

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,19 +3,20 @@ import styled, { css } from 'styled-components';
 // import { motion } from 'framer-motion';
 
 import { useStore } from '../store/useStore';
+import { NavigationSubtitle, NavigationTitle } from '../styles/TextStyles';
 
 // TODO: Add animation
 // TODO: Separate text styles and make a TextStyles.js file
 
-export default function Navigation({ title, path, index }) {
+export default function Navigation({ title, subtitle, path, position }) {
   const toggleOverlay = useStore((state) => state.toggleOverlay);
 
   return (
-    <LinkContainer index={index}>
+    <LinkContainer position={position}>
       <LinkWrapper>
         <Link to={path} onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay}>
-          <span>{title[0]}</span>
-          <span>{title[1]}</span>
+          <NavigationSubtitle>{subtitle}</NavigationSubtitle>
+          <NavigationTitle>{title}</NavigationTitle>
         </Link>
       </LinkWrapper>
     </LinkContainer>
@@ -36,6 +37,8 @@ const LinkContainer = styled.div`
   text-align: right;
   padding-left: 100px;
 
+  ${(props) => css`${props.position}`}
+
   &:hover {
     z-index: 25;
   }
@@ -49,111 +52,8 @@ const LinkContainer = styled.div`
     left: 0;
     top: 21px;
   }
-
-  span {
-    &:last-child {
-      font-family: 'Roboto Condensed', sans-serif;
-    }
-  }
-
-  ${(props) =>
-    props.index === 1
-      ? css`
-          left: calc(66.66% + 21px);
-          text-align: left;
-
-          span {
-            &:first-child {
-              text-align: right;
-              margin: 0 0 3px 0;
-              transform: translateX(100px);
-              color: rgba(255, 255, 255, 0);
-              color: rgba(255, 255, 255, 0.6);
-              transform: translateX(50px);
-            }
-
-            &:last-child {
-              margin: 0;
-              color: rgba(255, 255, 255, 0);
-              transform: translateX(-50px);
-              color: rgba(255, 255, 255, 0.5);
-              transform: translateX(0px);
-            }
-          }
-        `
-      : css`
-          left: calc(33.33% - 21px);
-          text-align: center;
-          padding: 0;
-
-          span {
-            &:first-child {
-              text-align: left;
-              margin: 0 0 3px 0;
-              transform: translateX(-100px);
-              color: rgba(255, 255, 255, 0);
-              color: rgba(255, 255, 255, 0.6);
-              transform: translateX(-50px);
-            }
-
-            &:last-child {
-              margin: 0;
-              color: rgba(255, 255, 255, 0);
-              transform: translateX(50px);
-              color: rgba(255, 255, 255, 0.5);
-              transform: translateX(0px);
-            }
-          }
-        `}
 `;
 
-// className='ashade-home-link is-link'
 const LinkWrapper = styled.div`
   margin: 0 auto;
-
-  span {
-    display: block;
-    transition: transform 0.5s, color 0.5s;
-
-    &:first-child {
-      display: block;
-      color: #5c5c60;
-      font-family: 'Montserrat', sans-serif;
-      font-weight: 700;
-      text-transform: uppercase;
-      font-size: 14px;
-      line-height: 18px;
-      margin: 0 0 -3px 0;
-    }
-
-    &:last-child {
-      font-family: 'Roboto', sans-serif;
-      font-weight: 700;
-      /* color: #ffffff; */
-      text-transform: uppercase;
-      font-size: 50px;
-      line-height: 55px;
-      margin: 0 0 28px 0;
-    }
-  }
-
-  &:hover span:first-child {
-    transform: translateX(-40px);
-    color: rgba(255, 255, 255, 0.8);
-  }
-
-  &:hover span:last-child {
-    transform: translateX(-10px);
-    color: #fff;
-  }
-
-  &:hover span:first-child {
-    color: rgba(255, 255, 255, 0.8);
-    transform: translateX(40px);
-  }
-
-  &:hover span:last-child {
-    transform: translateX(10px);
-    color: #fff;
-  }
 `;
