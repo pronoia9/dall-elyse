b76b7d49acdfa537a3121803c940670862c1745f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 22:38:59 +0300

    clear animation functions (make 1 big one)

--- client/src/utils/motion.js
+++ client/src/utils/motion.js
@@ -1,89 +1,101 @@
 import { easeInOut } from 'framer-motion';
 
+// DEFAULTS
+// x = 0, y = 0
+// delay = 0
+// duration = 0.5
+// ease = easeInOut
+
 // NAVBAR ANIMATIONS
-export const logoMotion = (count = 1) => slideInOut('left', 0.5 * count, 0.75, easeInOut);
+export const logoMotion = (count = 1) => bigMotion({ direction: 'left', delay: 0.5 * count, duration: 0.75 });
 
 export const navbarMotion = {
-  mobile: (count, delay = 0, duration = 0.5, ease = easeInOut) => ({
-    initial: { x: 100, opacity: 0 },
-    animate: { x: 0, opacity: 1, transition: { delay: 0, duration, ease } },
-    exit: { x: 100, opacity: 0, transition: { delay: (count - 1) * 0.5, duration, ease } },
-  }),
+  mobile: (count) => bigMotion({ delayOut: (count - 1) * 0.5 }),
 };
 export const navlinksMotion = {
-  desktop: (index, delay = 0.5, duration = 0.5, ease = easeInOut) => ({
-    initial: { x: -10, y: -10, opacity: 0 },
-    animate: { x: 0, y: 0, opacity: 1, transition: { delay, duration: duration * index, ease } },
-    exit: { x: -10, y: -10, opacity: 0, transition: { delay, duration: duration * index, ease } },
-  }),
-  mobile: (index, delay = 0.1, duration = 0.5, ease = easeInOut) => ({
-    initial: { y: 100, opacity: 0 },
-    animate: { y: 0, opacity: 1, transition: { delay, duration: duration * index, ease } },
-    exit: { y: -100, opacity: 0, transition: { delay, duration: duration * index, ease } },
-  }),
+  desktop: (index) => bigMotion({ direction: -10, duration: 0.5 * index }),
+  mobile: (index) => bigMotion({ directionIn: 'up', directionOut: 'down', delay: 0.1, duration: 0.5 * index }),
 };
 export const navigationMotion = {
   left: {
     animation: {},
     title1: {},
-    title2: {}
+    title2: {},
   },
   right: {
     animation: {},
     title1: {},
-    title2: {}
-  }
-}
+    title2: {},
+  },
+};
 
 // GENERAL ANIMATIONS
-export function fadeIn(delay = 0, duration = 0.5, ease = easeInOut) {
-  return {
-    initial: { opacity: 0 },
-    animate: { opacity: 1, transition: { delay, duration, ease } },
-    exit: { opacity: 0, transition: { delay, duration, ease } },
+export function bigMotion({
+  direction, directionIn, directionOut,
+  opacity, opacityIn, opacityOut,
+  type, typeIn, typeOut,
+  delay, delayIn, delayOut,
+  duration, durationIn, durationOut,
+  ease_, easeIn_, easeOut_,
+}) {
+  const options = {
+    initial: { x: 0, y: 0, opacity: 0 },
+    animate: { x: 0, y: 0, opacity: 1, transition: { type: '', delay: 0, duration: 0.5, ease: easeInOut } },
+    exit: { x: 0, y: 0, opacity: 0, transition: { type: '', delay: 0, duration: 0.5, ease: easeInOut } },
   };
-}
 
-export function fadeOut(delay = 0, duration = 0.5, ease = easeInOut) {
-  return {
-    initial: { opacity: 1 },
-    animate: { opacity: 0, transition: { delay, duration, ease } },
-    exit: { opacity: 1, transition: { delay, duration, ease } },
-  };
-}
+  // DIRECTION
+  if (direction) {
+    if (direction === 'up' || direction === 'top') (options.initial.y = 100), (options.exit.y = 100);
+    else if (direction === 'right') (options.initial.x = 100), (options.exit.x = 100);
+    else if (direction === 'down' || direction === 'bottom') (options.initial.y = -100), (options.exit.y = -100);
+    else if (direction === 'left') (options.initial.x = -100), (options.exit.x = -100);
+    else if (direction.x) (options.initial.x = direction.x)((options.exit.x = direction.x));
+    else if (direction.y) (options.initial.y = direction.y)((options.exit.y = direction.y));
+    else (options.initial.x = direction), (options.exit.x = direction), (options.initial.y = direction), (options.exit.y = direction);
+  }
+  if (directionIn) {
+    if (directionIn === 'up' || directionIn === 'top') options.initial.y = 100;
+    else if (directionIn === 'right') options.initial.x = 100;
+    else if (directionIn === 'down' || directionIn === 'bottom') options.initial.y = -100;
+    else if (directionIn === 'left') options.initial.x = -100;
+    else if (directionIn.x) (options.initial.x = directionIn.x)((options.exit.x = directionIn.x));
+    else if (directionIn.y) (options.initial.y = directionIn.y)((options.exit.y = directionIn.y));
+    else (options.initial.x = directionIn), (options.exit.x = directionIn), (options.initial.y = directionIn), (options.exit.y = directionIn);
+  }
+  if (directionOut) {
+    if (directionOut === 'up' || directionOut === 'top') options.exit.y = 100;
+    else if (directionOut === 'right') options.exit.x = 100;
+    else if (directionOut === 'down' || directionOut === 'bottom') options.exit.y = -100;
+    else if (directionOut === 'left') options.exit.x = -100;
+    else if (directionOut.x) (options.initial.x = directionOut.x)((options.exit.x = directionOut.x));
+    else if (directionOut.y) (options.initial.y = directionOut.y)((options.exit.y = directionOut.y));
+    else (options.initial.x = directionOut), (options.exit.x = directionOut), (options.initial.y = directionOut), (options.exit.y = directionOut);
+  }
 
-export function slideInOut(direction, delay = 0, duration = 0.5, ease = easeInOut, i = 1) {
-  return {
-    initial: {
-      x: direction === 'left' ? -100 : direction === 'right' ? 100 : 0,
-      y: direction === 'up' ? 100 : direction === 'down' ? 100 : 0,
-      opacity: 0,
-    },
-    animate: { x: 0, y: 0, opacity: 1, transition: { delay, duration: duration * i, ease } },
-    exit: {
-      x: direction === 'left' ? -100 : direction === 'right' ? 100 : 0,
-      y: direction === 'up' ? 100 : direction === 'down' ? 100 : 0,
-      opacity: 0,
-      transition: { delay, duration: duration * i, ease },
-    },
-  };
-}
+  // OPACITY
 
-export function slideInOut2(directionIn, directionOut, delay = 0, duration = 0.5, ease = easeInOut, i = 1) {
-  return {
-    initial: {
-      x: directionIn === 'left' ? -100 : directionIn === 'right' ? 100 : 0,
-      y: directionIn === 'up' ? 100 : directionIn === 'down' ? 100 : 0,
-      opacity: 0,
-    },
-    animate: { x: 0, y: 0, opacity: 1, transition: { delay, duration, ease } },
-    exit: {
-      x: directionOut === 'left' ? -100 : directionOut === 'right' ? 100 : 0,
-      y: directionOut === 'up' ? 100 : directionOut === 'down' ? 100 : 0,
-      opacity: 0,
-      transition: { delay, duration, ease },
-    },
-  };
+  // TYPE
+  if (type) options.animate.transition.type = type, options.exit.transition.type = type;
+  if (typeIn) options.animate.transition.type = typeIn;
+  if (typeOut) options.exit.transition.type = typeOut;
+
+  // DELAY
+  if (delay) (options.animate.transition.delay = delay), (options.exit.transition.delay = delay);
+  if (delayIn) options.animate.transition.delay = delayIn;
+  if (delayOut) options.exit.transition.delay = delayOut;
+
+  // DURATION
+  if (duration) (options.animate.transition.duration = duration), (options.exit.transition.duration = duration);
+  if (durationIn) options.animate.transition.duration = durationIn;
+  if (durationOut) options.exit.transition.duration = durationOut;
+
+  // EASE
+  if (ease_) (options.animate.transition.ease = ease_), (options.exit.transition.ease = ease_);
+  if (easeIn_) options.animate.transition.ease = easeIn_;
+  if (easeOut_) options.exit.transition.ease = easeOut_;
+
+  return options;
 }
 
 // export const textVariant = (delay) => {
