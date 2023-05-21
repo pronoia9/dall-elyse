cc8dea5ec9805b44ba7c937c024f624302deb99e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-21 17:37:14 +0300

    update + reorganize store

--- client/src/store/useStore.js
+++ client/src/store/useStore.js
@@ -1,10 +1,17 @@
 import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
 
-export const useStore = create((set) => ({
-  overlay: false,
-  toggleOverlay: () => set((state) => ({ overlay: !state.overlay })),
-  mobileMenuOpen: false,
-  enableMobileMenu: () => set({ mobileMenuOpen: true, overlay: true }),
-  enableMobileMenu: () => set({ mobileMenuOpen: false, overlay: false }),
-  toggleMobileMenu: () => set((state) => ({ mobileMenuOpen: !state.mobileMenuOpen, overlay: !state.overlay })),
-}));
+
+export const useStore = create(
+  subscribeWithSelector((set) => ({
+    // Overlay
+    overlay: false,
+    toggleOverlay: () => set((state) => ({ overlay: !state.overlay })),
+
+    // Mobile Menu
+    mobileMenuOpen: false,
+    openMobileMenu: () => set({ mobileMenuOpen: true, overlay: true }),
+    closeMobileMenu: () => set({ mobileMenuOpen: false, overlay: false }),
+    toggleMobileMenu: () => set((state) => ({ mobileMenuOpen: !state.mobileMenuOpen, overlay: !state.overlay })),
+  }))
+);
