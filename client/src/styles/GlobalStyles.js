9badc6fb0e920e98ac16fe16ca96a0fe6becd9d0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-22 02:22:04 +0300

    add global form styles

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -22,7 +22,65 @@ export const GlobalStyles = createGlobalStyle`
     font-weight: 500;
     color: #808080;
   }
+
   a, li {
     text-decoration: none;
+  }
+  
+  label {
+    font-size: 12px;
+    line-height: 16px;
+    margin: 0 0 5px 0;
+  }
+
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    height: 50px;
+    color: #808080;
+    border: 2px solid #313133;
+    border-radius: 0;
+    padding: 15px 20px 15px 20px;
+    margin: 0 0 20px 0;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    background: rgba(0, 0, 0, 100);
+    transition: background 0.3s, border 0.3s;
+  }
+
+  textarea {
+    resize: none;
+    line-height: 24px;
+    height: 230px;
+  }
+
+  button,
+  input[type='button'],
+  input[type='reset'],
+  input[type='submit'] {
+    display: inline-block;
+    width: auto;
+    padding: 15px 40px 15px 40px;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+    color: #ffffff;
+    text-transform: uppercase;
+    font-size: 14px;
+    line-height: 16px;
+    cursor: pointer;
+    border: 2px solid #313133;
+    background: rgba(0, 0, 0, 100);
+    transition: background 0.3s, border 0.3s, color 0.3s;
+  }
+
+  button:hover,
+  input[type='button']:hover,
+  input[type='reset']:hover,
+  input[type='submit']:hover {
+    border-color: #ffffff;
   }
 `;
