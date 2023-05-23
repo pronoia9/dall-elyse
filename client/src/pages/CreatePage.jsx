ea0aedc2abb50e74e850977822048235463b2e4e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-23 18:56:10 +0300

    reset styles

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -30,8 +30,9 @@ const CreatePage = () => {
               {/* Prompt + Prompt Button */}
               <textarea placeholder='A futuristic cyborg dance club, neon lights' />
               <Buttons className='Buttons'>
-                <button>Share</button>
                 <button>Generate</button>
+                <button>Surprise Me</button>
+                <button>Share</button>
               </Buttons>
             </Form>
           </FormContainer>
@@ -43,89 +44,20 @@ const CreatePage = () => {
 };
 export default CreatePage;
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.85);
-`;
+const Container = styled.div``;
 
-const Wrapper = styled.div`
-  width: calc(100% - 374px);
-  max-width: 1280px;
-  min-height: 100%;
-  height: 100vh;
-  margin: auto;
-  padding-top: 100px;
-  padding-bottom: 90px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
+const Wrapper = styled.div``;
 
-const Row = styled.div`
-  width: 100%;
-  height: 100%;
-  margin: 0 -20px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+const Row = styled.div``;
 
-  &:last-child {
-    gap: 2%;
-  }
-`;
+const TextContainer = styled.div``;
 
-const TextContainer = styled.div`
-  width: 75%;
-  text-align: center;
-  /* margin-bottom: 80px; */
-`;
+const ImageContainer = styled.div``;
 
-const ImageContainer = styled.div`
-  width: calc(33.33% - 40px);
-  height: 100%;
-`;
+const ImageWrapper = styled.div``;
 
-const ImageWrapper = styled.div`
-  background: url(${(props) => (props.image ? props.image : '')}) center center #000;
-  background-size: ${(props) => props.image && 'cover'};
-  height: 100%;
-  width: 100%;
-`;
-
-const FormContainer = styled.div`
-  width: calc(66.66% - 40px);
-
-  input,
-  textarea,
-  button {
-    &:hover {
-      border: 2px solid rgba(255, 255, 255, 0.5);
-      background: rgba(0, 0, 0, 0.5);
-    }
-  }
-`;
+const FormContainer = styled.div``;
 
 const Form = styled.div``;
 
-const Buttons = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  gap: 5%;
-
-  button {
-    width: 100%;
-  }
-
-  @media only screen and (max-width: 960px) {
-    flex-direction: column-reverse;
-
-    button {
-      &:last-child {
-        margin-bottom: 10px;
-      }
-    }
-  }
-`;
+const Buttons = styled.div``;
