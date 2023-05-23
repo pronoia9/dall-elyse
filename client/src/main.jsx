e69b00bd1e64dd03c82f742d8c5a9f4d4dfa0d17
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-24 01:40:24 +0300

    add fontawesome css locally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import './styles/fontawesome.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
