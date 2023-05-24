f5d9f8f3e1a33bd6561a9b38ba7d9d758de61d1b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-24 21:50:51 +0300

    update navigation text styles

--- client/src/styles/TextStyles.js
+++ client/src/styles/TextStyles.js
@@ -9,13 +9,28 @@ export const NavigationSubtitle = styled(motion.span)`
   text-transform: uppercase;
   font-size: 14px;
   line-height: 18px;
-  margin: 0 0 -3px 0;
+  /* margin: 0 0 -3px 0; */
+  margin: 0 0 3px 0;
   transition: transform 0.5s, color 0.5s;
+
+  @media only screen and (max-width: 960px) {
+    text-align: center !important;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 12px;
+    line-height: 16px;
+  }
 `;
 
 export const NavigationTitle = styled(NavigationSubtitle)`
+  color: rgba(255, 255, 255, 0.5);
   font-family: 'Roboto', sans-serif;
   font-size: 50px;
   line-height: 55px;
-  margin: 0 0 28px 0;
+  /* margin: 0 0 28px 0; */
+  margin: 0;
+
+  @media only screen and (max-width: 960px) {
+    font-size: 30px;
+    line-height: 35px;
+  }
 `;
