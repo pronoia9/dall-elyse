406af86a13de999218a98f5c1cdaa8d3a3f630f7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-25 20:56:26 +0300

    add initial layout

--- client/src/components/GalleryCard.jsx
+++ client/src/components/GalleryCard.jsx
@@ -1,4 +1,16 @@
-const GalleryCard = () => {
-  return <div>GalleryCard</div>;
+import styled from 'styled-components';
+
+const GalleryCard = ({ _id, name, prompt, photo }) => {
+  return (
+    <Container className='galleryCard-container'>
+      <Wrapper className='galleryCard-wrapper'>
+        <img src={photo} />
+      </Wrapper>
+    </Container>
+  );
 };
 export default GalleryCard;
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
