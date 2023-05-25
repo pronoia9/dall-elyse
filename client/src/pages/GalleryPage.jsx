11892f34dc86fe02191bbc88cc78297e63f00834
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-25 20:50:23 +0300

    add initial gallery page layout

--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -1,16 +1,35 @@
 import styled from 'styled-components';
 
-import { Navigation } from '../components';
+import { GalleryCard, Navigation } from '../components';
 import ContentWrapper from '../hoc/ContentWrapper';
 import { navigationData } from '../utils/data';
+import { useStore } from '../store/useStore';
 
 const GalleryPage = () => {
+  const data = useStore((state) => state.data);
+  console.log(data);
+
   return (
-    <Container>
+    <Container id='gallery-page' className='galleryPage-container'>
       {/* Navigation Links */}
-      <Navigation {...navigationData.return} {...{ position: left, center: true }} />
-      <Navigation {...navigationData.gallery} {...{ position: right, center: false }} />
+      {Object.values(navigationData.galleryPage).map((link) => (
+        <Navigation key={`navigation-${link.title}`} {...link} />
+      ))}
+
+      <Wrapper className='galleryPage-wrapper'>
+        <Grid className='galleryPage-grid'>
+          {data?.map((d, i) => (
+            <GalleryCard key={`card-${i}${d.id}`} {...d} />
+          ))}
+        </Grid>
+      </Wrapper>
     </Container>
   );
 };
 export default ContentWrapper(GalleryPage, 'gallery');
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const Grid = styled.div``;
