d996ed866a12346f8ce52de7f0ca1075ee9544e1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-26 02:22:34 +0300

    add photoswipe

--- client/src/store/useStore.js
+++ client/src/store/useStore.js
@@ -1,9 +1,12 @@
 import { create } from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
 
-
 export const useStore = create(
   subscribeWithSelector((set) => ({
+    // Loading
+    isLoading: true,
+    data: [],
+
     // Overlay
     overlay: false,
     toggleOverlay: () => set((state) => ({ overlay: !state.overlay })),
@@ -13,5 +16,18 @@ export const useStore = create(
     openMobileMenu: () => set({ mobileMenuOpen: true, overlay: true }),
     closeMobileMenu: () => set({ mobileMenuOpen: false, overlay: false }),
     toggleMobileMenu: () => set((state) => ({ mobileMenuOpen: !state.mobileMenuOpen, overlay: !state.overlay })),
+
+    // Create Page
+    isGenerating: false,
+
+    // Gallery Page
+    photoSwipe: null,
+    setPhotoSwipe: (index) => set((state) => ({ photoSwipe: { ...state.data[index], index: index } })),
+    photoSwipePrev: () =>
+      set(({ data, photoSwipe }) => (photoSwipe.index > 0 ? { photoSwipe: { ...data[photoSwipe.index - 1], index: photoSwipe.index - 1 } } : {})),
+    photoSwipeNext: () =>
+      set(({ data, photoSwipe }) =>
+        photoSwipe.index < data.length - 1 ? { photoSwipe: { ...data[photoSwipe.index + 1], index: photoSwipe.index + 1 } } : {}
+      ),
   }))
 );
