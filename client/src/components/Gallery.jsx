20efbea38805168cb94cbe653958d2855133e07f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-25 20:53:47 +0300

    move gallery content to gallery component

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,4 +1,24 @@
+import styled from 'styled-components';
+
+import GalleryCard from './GalleryCard';
+import { useStore } from '../store/useStore';
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const data = useStore((state) => state.data);
+  console.log(data);
+
+  return (
+    <Container className='gallery-container'>
+      <Wrapper className='gallery-wrapper'>
+        {data?.map((d, i) => (
+          <GalleryCard key={`card-${i}${d.id}`} {...d} />
+        ))}
+      </Wrapper>
+    </Container>
+  );
 };
 export default Gallery;
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -1,35 +1,19 @@
 import styled from 'styled-components';
 
-import { GalleryCard, Navigation } from '../components';
+import { Gallery, Navigation } from '../components';
 import ContentWrapper from '../hoc/ContentWrapper';
 import { navigationData } from '../utils/data';
-import { useStore } from '../store/useStore';
 
 const GalleryPage = () => {
-  const data = useStore((state) => state.data);
-  console.log(data);
-
   return (
     <Container id='gallery-page' className='galleryPage-container'>
       {/* Navigation Links */}
-      {Object.values(navigationData.galleryPage).map((link) => (
-        <Navigation key={`navigation-${link.title}`} {...link} />
-      ))}
-
-      <Wrapper className='galleryPage-wrapper'>
-        <Grid className='galleryPage-grid'>
-          {data?.map((d, i) => (
-            <GalleryCard key={`card-${i}${d.id}`} {...d} />
-          ))}
-        </Grid>
-      </Wrapper>
+      {Object.values(navigationData.galleryPage).map((link) => <Navigation key={`navigation-${link.title}`} {...link} />)}
+      {/* Gallery Grid */}
+      <Gallery />
     </Container>
   );
 };
 export default ContentWrapper(GalleryPage, 'gallery');
 
 const Container = styled.div``;
-
-const Wrapper = styled.div``;
-
-const Grid = styled.div``;
