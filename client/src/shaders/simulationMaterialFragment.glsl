fa2d1b3e29edd58143edff6cea74d6dc41c67573
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-30 02:06:31 +0300

    format

--- client/src/shaders/simulationMaterialFragment.glsl
+++ client/src/shaders/simulationMaterialFragment.glsl
@@ -15,21 +15,11 @@ varying vec2 vUv;
 // Copyright (c) 2011 Stefan Gustavson. All rights reserved.
 // Distributed under the MIT license. See LICENSE file.
 // https://github.com/ashima/webgl-noise
-vec3 mod289(vec3 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-vec4 mod289(vec4 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-vec4 permute(vec4 x) {
-  return mod289(((x * 34.0) + 1.0) * x);
-}
-vec4 taylorInvSqrt(vec4 r) {
-  return 1.79284291400159 - 0.85373472095314 * r;
-}
-vec3 fade(vec3 t) {
-  return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
-}
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec4 permute(vec4 x) { return mod289(((x * 34.0) + 1.0) * x); }
+vec4 taylorInvSqrt(vec4 r) { return 1.79284291400159 - 0.85373472095314 * r; }
+vec3 fade(vec3 t) { return t * t * t * (t * (t * 6.0 - 15.0) + 10.0); }
 // Classic Perlin noise
 float noise(vec3 P) {
   vec3 Pi0 = floor(P); // Integer part for indexing
@@ -97,11 +87,11 @@ float snoise(vec3 v) {
   const vec2 C = vec2(1.0 / 6.0, 1.0 / 3.0);
   const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
 
-// First corner
+  // First corner
   vec3 i = floor(v + dot(v, C.yyy));
   vec3 x0 = v - i + dot(i, C.xxx);
 
-// Other corners
+  // Other corners
   vec3 g = step(x0.yzx, x0.xyz);
   vec3 l = 1.0 - g;
   vec3 i1 = min(g.xyz, l.zxy);
@@ -115,12 +105,12 @@ float snoise(vec3 v) {
   vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
 
-// Permutations
+  // Permutations
   i = mod289(i);
   vec4 p = permute(permute(permute(i.z + vec4(0.0, i1.z, i2.z, 1.0)) + i.y + vec4(0.0, i1.y, i2.y, 1.0)) + i.x + vec4(0.0, i1.x, i2.x, 1.0));
 
-// Gradients: 7x7 points over a square, mapped onto an octahedron.
-// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+  // Gradients: 7x7 points over a square, mapped onto an octahedron.
+  // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
   float n_ = 0.142857142857; // 1.0/7.0
   vec3 ns = n_ * D.wyz - D.xzx;
 
@@ -150,31 +140,28 @@ float snoise(vec3 v) {
   vec3 p2 = vec3(a1.xy, h.z);
   vec3 p3 = vec3(a1.zw, h.w);
 
-//Normalise gradients
+  //Normalise gradients
   vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
   p3 *= norm.w;
 
-// Mix final noise value
+  // Mix final noise value
   vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
   m = m * m;
   return 42.0 * dot(m * m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
 }
 
 vec3 snoiseVec3(vec3 x) {
-
   float s = snoise(vec3(x));
   float s1 = snoise(vec3(x.y - 19.1, x.z + 33.4, x.x + 47.2));
   float s2 = snoise(vec3(x.z + 74.2, x.x - 124.5, x.y + 99.4));
   vec3 c = vec3(s, s1, s2);
   return c;
-
 }
 
 vec3 curl(vec3 p) {
-
   const float e = .1;
   vec3 dx = vec3(e, 0.0, 0.0);
   vec3 dy = vec3(0.0, e, 0.0);
@@ -193,7 +180,6 @@ vec3 curl(vec3 p) {
 
   const float divisor = 1.0 / (2.0 * e);
   return normalize(vec3(x, y, z) * divisor);
-
 }
 
 // MAIN
@@ -207,5 +193,6 @@ void main() {
   curlPos += curl(curlPos * uCurlFreq * 4.0) * 0.25;
   curlPos += curl(curlPos * uCurlFreq * 8.0) * 0.125;
   curlPos += curl(pos * uCurlFreq * 16.0) * 0.0625;
+
   gl_FragColor = vec4(mix(pos, curlPos, noise(pos + t)), 1.0);
 }
