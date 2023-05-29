fd52a94a9b2e152bc18b607fb81285f44a3b18b7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-30 00:53:44 +0300

    install R3F libraries

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import glsl from 'vite-plugin-glsl';
+import glsl from 'babel-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), glsl()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  define: {
+    'process.env': process.env,
+    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+  },
+});
