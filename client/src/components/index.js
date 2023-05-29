7ccd300a8885ff7cfbd21c065f4aca2c8573f586
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-29 19:16:14 +0300

    add/init loading component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,7 +3,8 @@ import Gallery from './Gallery';
 import GalleryCard from './GalleryCard';
 import GalleryCardOverlay from './GalleryCardOverlay';
 import Loader from './Loader';
+import Loading from './Loading';
 import Navbar from './Navbar';
 import Navigation from './Navigation';
 
-export { Canvas, Gallery, GalleryCard, GalleryCardOverlay, Loader, Navbar, Navigation };
+export { Canvas, Gallery, GalleryCard, GalleryCardOverlay, Loader, Loading, Navbar, Navigation };
