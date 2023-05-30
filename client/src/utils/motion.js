07b8a9f66cfd3f3f03e46e8f43b2b11fce1886d4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-30 22:33:16 +0300

    convert mobile menu + links & remove bigmotion

--- client/src/utils/motion.js
+++ client/src/utils/motion.js
@@ -2,21 +2,21 @@ import merge from 'lodash.merge';
 import { easeInOut } from 'framer-motion';
 
 /*******************************************  APP START  *******************************************/
-// PAGE WRAPPER (delay: 0) (duration: 0.5s)
+// PAGE WRAPPER (delay: 0) (duration: 0.5)
 export const contentMotion = () => ({
   initial: { opacity: 0 },
   animate: { opacity: 1, transition: { type: 'tween', delay: 0, duration: 0.5, ease: easeInOut } },
   exit: { opacity: 0 },
 });
 
-// CIRCLE BACKGROUND (delay: 0) (duration: 0.5s)
+// CIRCLE BACKGROUND (delay: 0) (duration: 0.5)
 export const circleMotion = () => ({
   initial: { x: -500, y: -500 },
   animate: { x: 0, y: 0, transition: { type: 'tween', duration: 0.5, ease: easeInOut } },
   exit: { x: 500, y: 500 }, // added key to location so exit animation happens between pages
 });
 
-// NAVIGATION ANIMATIONS (delay: 0.5) (duration: 0.5) (total duration: 1s)
+// NAVIGATION ANIMATIONS (delay: 0.5) (duration: 0.5) (total duration: 1)
 export const navigationMotion = {
   transition: { type: 'tween', delay: 0.5, duration: 0.5, ease: 'linear' },
   lineMotion: (center) => {
@@ -63,13 +63,27 @@ export const navbarMotion = {
   // DESKTOP NAVBAR (delay: 1) (duration: 0.25 * 3 = 0.75) (total duration: 1.75)
   desktopLinksMotion: (index) => ({
     initial: { x: -25, y: -25, opacity: 0 },
-    animate: { x: 0, y: 0, opacity: 1, transition: { type: 'tween', delay: 1, duration: 0.25 * index, ease: easeInOut },
-    },
+    animate: { x: 0, y: 0, opacity: 1, transition: { type: 'tween', delay: 1, duration: 0.25 * index, ease: easeInOut } },
     exit: { x: -25, y: -25, opacity: 0 },
   }),
-  // MOBILE MENU
-  mobileMenuMotion: (count = 3) => bigMotion({ direction: { x: window.screen.width }, delayOut: 0.25 * count, opacity: 1 }),
-  mobileLinksMotion: (index) => bigMotion({ directionIn: 'up', directionOut: 'down', delayIn: 0.5, duration: 0.25 * index }),
+  // MOBILE MENU (delay: 0 || 0.25 * 3 = 0.75) (duration: 0.5) (total duration: 0.5 || 1.25)
+  mobileMenuMotion: (count = 3) => {
+    const transition = { type: 'tween', delay: 0, duration: 0.5, ease: easeInOut };
+    return {
+      initial: { x: window.screen.width },
+      animate: { x: 0, transition },
+      exit: { x: window.screen.width, transition: { ...transition, delay: 0.25 * count } },
+    };
+  },
+  // MOBILE MENU LINKS (delay: 0.5 || 0) (duration: 0.25 * 3 = 0.75) (total duration: 1.25 || 0.75)
+  mobileLinksMotion: (index) => {
+    const transition = { type: 'tween', delay: 0.5, duration: 0.25 * index, ease: easeInOut };
+    return {
+      initial: { y: 100, opacity: 0 },
+      animate: { y: 0, opacity: 1, transition },
+      exit: { y: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+    };
+  },
 };
 /********************************************  APP END  ********************************************/
 
@@ -95,88 +109,6 @@ export const canvasMotion = () => ({
 /****************************************  GALLERY PAGE END  ***************************************/
 
 /******************************************  UTILS START  ******************************************/
-export function bigMotion(props = {}, extra = {}) {
-  const {
-    direction,
-    directionIn,
-    directionOut,
-    opacity,
-    opacityIn,
-    opacityOut,
-    type,
-    typeIn,
-    typeOut,
-    delay,
-    delayIn,
-    delayOut,
-    duration,
-    durationIn,
-    durationOut,
-    ease_,
-    easeIn_,
-    easeOut_,
-  } = props;
-  const options = {
-    initial: { x: 0, y: 0, opacity: 0 },
-    animate: { x: 0, y: 0, opacity: 1, transition: { type: '', delay: 0, duration: 0.5, ease: easeInOut } },
-    exit: { x: 0, y: 0, opacity: 0, transition: { type: '', delay: 0, duration: 0.5, ease: easeInOut } },
-  };
-
-  // DIRECTION
-  if (direction) {
-    if (direction === 'up' || direction === 'top') (options.initial.y = 100), (options.exit.y = 100);
-    else if (direction === 'right') (options.initial.x = 100), (options.exit.x = 100);
-    else if (direction === 'down' || direction === 'bottom') (options.initial.y = -100), (options.exit.y = -100);
-    else if (direction === 'left') (options.initial.x = -100), (options.exit.x = -100);
-    else if (direction.x) (options.initial.x = direction.x), (options.exit.x = direction.x);
-    else if (direction.y) (options.initial.y = direction.y), (options.exit.y = direction.y);
-    else (options.initial.x = direction), (options.exit.x = direction), (options.initial.y = direction), (options.exit.y = direction);
-  }
-  if (directionIn) {
-    if (directionIn === 'up' || directionIn === 'top') options.initial.y = 100;
-    else if (directionIn === 'right') options.initial.x = 100;
-    else if (directionIn === 'down' || directionIn === 'bottom') options.initial.y = -100;
-    else if (directionIn === 'left') options.initial.x = -100;
-    else if (directionIn.x) (options.initial.x = directionIn.x), (options.exit.x = directionIn.x);
-    else if (directionIn.y) (options.initial.y = directionIn.y), (options.exit.y = directionIn.y);
-    else (options.initial.x = directionIn), (options.exit.x = directionIn), (options.initial.y = directionIn), (options.exit.y = directionIn);
-  }
-  if (directionOut) {
-    if (directionOut === 'up' || directionOut === 'top') options.exit.y = 100;
-    else if (directionOut === 'right') options.exit.x = 100;
-    else if (directionOut === 'down' || directionOut === 'bottom') options.exit.y = -100;
-    else if (directionOut === 'left') options.exit.x = -100;
-    else if (directionOut.x) (options.initial.x = directionOut.x), (options.exit.x = directionOut.x);
-    else if (directionOut.y) (options.initial.y = directionOut.y), (options.exit.y = directionOut.y);
-    else (options.initial.x = directionOut), (options.exit.x = directionOut), (options.initial.y = directionOut), (options.exit.y = directionOut);
-  }
-
-  // OPACITY
-  if (opacity) (options.initial.opacity = opacity), (options.animate.opacity = opacity), (options.exit.opacity = opacity);
-
-  // TYPE
-  if (type) (options.animate.transition.type = type), (options.exit.transition.type = type);
-  if (typeIn) options.animate.transition.type = typeIn;
-  if (typeOut) options.exit.transition.type = typeOut;
-
-  // DELAY
-  if (delay) (options.animate.transition.delay = delay), (options.exit.transition.delay = delay);
-  if (delayIn) options.animate.transition.delay = delayIn;
-  if (delayOut) options.exit.transition.delay = delayOut;
-
-  // DURATION
-  if (duration) (options.animate.transition.duration = duration), (options.exit.transition.duration = duration);
-  if (durationIn) options.animate.transition.duration = durationIn;
-  if (durationOut) options.exit.transition.duration = durationOut;
-
-  // EASE
-  if (ease_) (options.animate.transition.ease = ease_), (options.exit.transition.ease = ease_);
-  if (easeIn_) options.animate.transition.ease = easeIn_;
-  if (easeOut_) options.exit.transition.ease = easeOut_;
-
-  return merge(options, extra);
-}
-
 export const textVariant = (delay) => ({
   initial: 'initial',
   animate: 'animate',
