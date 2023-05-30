f5ffdd9e738c965d18a7ce80922c7c2a7837c0bb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-31 00:48:31 +0300

    remove unused imports and add gallery page motion

--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-import { Gallery, Navigation } from '../components';
-import { navigationData } from '../utils/data';
+import { Gallery } from '../components';
 import { useStore } from '../store/useStore';
+import { galleryPageMotion } from '../utils/motion';
 
 export default function GalleryPage() {
   const searchKey = useStore((state) => state.searchKey),
     setSearchKey = useStore((state) => state.setSearchKey);
 
   return (
-    <Container>
+    <Container {...galleryPageMotion()}>
       {/* Searchbar maybe? */}
       <InputWrapper>
         <input type='text' name='searchKey' value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
@@ -21,7 +22,7 @@ export default function GalleryPage() {
   );
 }
 
-const Container = styled.div`
+const Container = styled(motion.section)`
   width: 100%;
 `;
 
