aa393165213499937e5f8ef3e0a9715b6a336083
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-31 20:33:43 +0300

    add init container styles

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,4 +1,16 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return <Container>Loading</Container>;
 };
 export default Loading;
+
+const Container = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: red;
+`;
