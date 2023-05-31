d893fda72a35791e9bd243b0f8fa19767ee8675a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-31 21:03:27 +0300

    add init overlay + handle hover

--- client/src/components/GalleryCard.jsx
+++ client/src/components/GalleryCard.jsx
@@ -11,8 +11,16 @@ const GalleryCard = ({ _id, name, prompt, photo, index, setImagesLoaded }) => {
   const setPhotoSwipe = useStore((state) => state.setPhotoSwipe);
   // STATES
   const [loading, setLoading] = useState(true);
+  const [hover, setHover] = useState(false);
   
   const handleClick = () => { setPhotoSwipe(index); }
+
+  const handleHover = (e) => {
+    if (!loading) {
+      e.type === 'mouseenter' && setHover(true);
+      e.type === 'mouseleave' && setHover(false);
+    }
+  }
   
   const imageLoaded = () => {
     setImagesLoaded((prev) => ([prev[0] + 1, prev[1]]));
@@ -20,9 +28,16 @@ const GalleryCard = ({ _id, name, prompt, photo, index, setImagesLoaded }) => {
   }
   
   return (
-    <Container key={`card-${_id}`} onClick={handleClick} {...galleryCardMotion(index, loading)}>
+    <Container key={`card-${_id}`} onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover} {...galleryCardMotion(index, loading)}>
       {loading && <Loading />}
       <motion.img key={`cardimage-${_id}`} src={photo} onLoad={imageLoaded} {...galleryCardImageMotion(index, loading)} />
+      <Overlay hover={hover}>
+        <TextContainer>
+          <p>{name}</p>
+          <p>{prompt}</p>
+        </TextContainer>
+
+      </Overlay>
     </Container>
   );
 };
@@ -58,3 +73,16 @@ const Container = styled(motion.div)`
     width: 100%;
   }
 `;
+
+const Overlay = styled.div`
+  display: ${(props) => !props.hover && 'none'};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #00000085;
+
+`;
+
+const TextContainer = styled.div``;
