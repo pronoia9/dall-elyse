7b9da752c1f09de643c3eb0aafde6ba6a059e4c8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-01 23:34:09 +0300

    add some photoswipe styles + settings

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -5,6 +5,7 @@ import { Gallery as PhotoSwipeGallery } from 'react-photoswipe-gallery';
 import { useStore } from '../store/useStore';
 import { GalleryCard } from './';
 import 'photoswipe/dist/photoswipe.css';
+import '../styles/default-skin.css';
 
 export default function Gallery() {
   // STORE
@@ -13,6 +14,7 @@ export default function Gallery() {
   // LOCAL STATE (FILTERED DATA)
   const [filteredData, setFilteredData] = useState(data);
 
+  // Check if input includes the search key
   const check = (a) => `${a}`.toLowerCase().includes(`${searchKey}`.toLowerCase());
 
   // SET SEARCH KEY WITH A 0.5s DELAY
@@ -22,9 +24,18 @@ export default function Gallery() {
     }, 500);
   }, [searchKey]);
 
+  // PHOTOSWIPE OPTIONS
+  const options = {
+    zoom: false,
+    close: true,
+    counter: true,
+    bgOpacity: 0.75,
+    pswpModule: () => import('photoswipe'),
+  };
+
   return (
     <Container>
-      <PhotoSwipeGallery withCaption withDownloadButton>
+      <PhotoSwipeGallery withDownloadButton withCaption options={options}>
         {filteredData?.map((d, index) => (
           <GalleryCard key={`card-${d._id}`} {...d} index={index} />
         ))}
@@ -37,10 +48,6 @@ const Container = styled.div`
   margin: -20px;
   display: flex;
   flex-wrap: wrap;
-
-  @media only screen and (max-width: 1200px) {
-    margin: -15px;
-  }
 
   @media only screen and (max-width: 760px) {
     margin: -15px 0;
