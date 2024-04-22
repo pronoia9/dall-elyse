5600952d3a2228b761e5491b8395dcc71f4caadd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-22 14:08:50 -0400

    add index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Gallery } from './Gallery';
+export { default as GalleryCard } from './GalleryCard';
+export { default as Loading } from './Loading';
+export { default as Navbar } from './Navbar';
+export { default as Navigation } from './Navigation';
+export { default as ParticlesCanvas } from './ParticlesCanvas';
+export { default as Preloader } from './Preloader';
