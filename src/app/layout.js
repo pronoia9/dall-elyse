c171d1d4fad512f4d819a0319eeb5cc32093dc33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-23 16:32:26 -0400

    add container

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 import { useStore } from '@/store';
 import { GlobalStyles, StyledComponentsRegistry } from '@/styles';
@@ -36,9 +38,96 @@ export default function RootLayout({ children }) {
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' />
       </head>
       <StyledComponentsRegistry>
-        <GlobalStyles />
-        <body>{children}</body>
+        <body>
+          <Container>
+            <GlobalStyles />
+            {children}
+          </Container>
+        </body>
       </StyledComponentsRegistry>
     </html>
   );
 }
+
+const Container = styled.div`
+  z-index: 5;
+  position: fixed;
+  width: 100%;
+  height: calc(100% - 155px);
+  left: 0;
+  bottom: 0;
+  display: block;
+  background: linear-gradient(-90deg, rgba(0, 0, 0, 0) 0%, #000 150%);
+  transition: transform 0.5s;
+
+  overflow: hidden;
+  scroll-behavior: smooth;
+
+  @media only screen and (max-width: 960px) {
+    height: calc(100% - 115px);
+  }
+`;
+
+const Circle = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  opacity: 0.85;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -999;
+
+  &:before {
+    content: '';
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(ellipse at left top, #28282e 0%, #000000 65%);
+    z-index: -999;
+  }
+`;
+
+const Main = styled(motion.main)`
+  width: calc(100% - 374px);
+  max-width: 1280px;
+  height: 100%;
+  /* padding-top: 50px; */
+  /* padding-bottom: 40px; */
+  margin: auto;
+  overflow: scroll;
+
+  & > div:last-child {
+    margin-bottom: 0;
+  }
+
+  @media only screen and (max-width: 1800px) {
+    width: calc(100% - 352px);
+  }
+
+  @media only screen and (max-width: 1600px) {
+    width: calc(100% - 312px);
+  }
+
+  @media only screen and (max-width: 1200px) {
+    width: calc(100% - 272px);
+  }
+
+  @media only screen and (max-width: 960px) {
+    width: calc(100% - 176px);
+    margin: auto 40px auto auto;
+    /* width: calc(100% - 280px);
+    margin: auto; */
+  }
+
+  @media only screen and (max-width: 760px) {
+    width: calc(100% - 40px);
+    margin: auto;
+    /* margin: auto auto auto 20px;
+    width: calc(100% - 95px); */
+    /* width: 100%;
+    margin: auto; */
+  }
+`;
+
