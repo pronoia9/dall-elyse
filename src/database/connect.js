88b98577b886195b3ebcb4bb712ab154b875a908
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-24 10:36:56 -0400

    format + add check to env variable

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,20 +1,26 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; // track the connection
+let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
+  // Set strict query mode for Mongoose to prevent unknown field queries.
   mongoose.set('strictQuery', true);
 
+  if (!process.env.MONGODB_URL) {
+    console.log('Missing MongoDB URL');
+    return;
+  }
+
+  // If the connection is already established, return without creating a new connection.
   if (isConnected) {
     console.log('MongoDB is already connected.');
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL).then(() => {
-      isConnected = true;
-      console.log('MongoDB connected.');
-    }); //.catch((error) => void console.error('MongoDB connection error:', error));
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true; // Set the connection status to true
+    console.log('MongoDB connected.');
   } catch (error) {
     console.error('MongoDB connection error:', error);
   }
