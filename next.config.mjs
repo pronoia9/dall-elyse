aa54491c4ac0fb6b49fad0430eb0ae4f101885d7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-24 10:42:09 -0400

    add experimental config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     styledComponents: true,
   },
   reactStrictMode: false,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ['mongoose'],
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.glsl/,
