980ab4d0b3d7510b1627db376eb519f25c69673c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-23 21:53:10 -0400

    use preloader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,6 +1,6 @@
-import Loader from "@/components";
+import { Preloader } from "@/components";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <Loader />;
+  return <Preloader />;
 }
