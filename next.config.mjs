e34f719f711d82f7ac548a205c46ed65f20662c2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-23 21:47:49 -0400

    edit nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  reactStrictMode: false,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.glsl/,
+      type: 'asset/source',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
